01515caf7102231094a2658e83b594c5557f5116
Bruno Guedes <brunoguedesit@gmail.com>
2021-01-29 12:04:16 -0300

    Testing Create Trainer module

---
+++ test/ex_mon/trainer/create_test.exs
@@ -0,0 +1,29 @@
+defmodule ExMon.Trainer.CreateTest do
+  use ExMon.DataCase
+
+  alias ExMon.{Trainer, Repo}
+  alias Trainer.Create
+
+  describe "call/1" do
+    test "when all params are valid, creates a trainer" do
+      params = %{name: "Bruno", password: "123456"}
+
+      count_before = Repo.aggregate(Trainer, :count)
+
+      response = Create.call(params)
+
+      count_after = Repo.aggregate(Trainer, :count)
+
+      assert {:ok, %Trainer{name: "Bruno", password: "123456"}} = response
+      assert count_after > count_before
+    end
+    test "when there are invalid params, returns the error" do
+      params = %{name: "Bruno"}
+
+      response = Create.call(params)
+
+      assert {:error, changeset} = response
+      assert errors_on(changeset) == %{password: ["can't be blank"]}
+    end
+  end
+end
